77181697e3dcda4f981226f629cd27d8cd8034d1
Diego Ducatti <118085900+dducatti@users.noreply.github.com>
2023-05-27 12:21:45 -0300

    Update graficos_inflacao.py

--- graficos_inflacao.py
+++ graficos_inflacao.py
@@ -12,31 +12,35 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_rows', 1000)
 
 
+## Obtendo dados:
 ipca = pd.DataFrame(sgs.get({'IPCA': 433}, start='2000-01-01'))
+igpm = pd.DataFrame(sgs.get({'IGPM': 189}, start='2000-01-01'))
+ippa = pd.DataFrame(sgs.get({'IPPA': 225}, start='2000-01-01'))
+
+
+## Tratando os dados:
 ipca['IPCA'] = (1 + ipca['IPCA'] / 100)
 ipca_acum = (ipca.cumprod() - 1) * 100
-igpm = pd.DataFrame(sgs.get({'IGPM': 189}, start='2000-01-01'))
-igpm.drop(['2023-04-01'], inplace=True)   # para igualar a qtd de dados
+
+#igpm.drop(['2023-04-01'], inplace=True)   # iguala a qtd de dados em caso faltante
 igpm['IGPM'] = (1 + igpm['IGPM'] / 100)
 igpm_acum = (igpm.cumprod() - 1) * 100
-ippa = pd.DataFrame(sgs.get({'IPPA': 225}, start='2000-01-01'))
-ippa.drop(['2023-04-01'], inplace=True)   # para igualar a qtd de dados
+
+#ippa.drop(['2023-04-01'], inplace=True)   # iguala a qtd de dados em caso faltante
 ippa['IPPA'] = (1 + ippa['IPPA']/ 100)
 ippa_acum = (ippa.cumprod() - 1) * 100
 
-
-
+# Gerando o DataFrame
 indice = pd.DataFrame([igpm_acum['IGPM'], ipca_acum['IPCA'],
                        ippa_acum['IPPA']]).T
-print(indice)
 
+# Plot
 fig, img = plt.subplots()
 img.plot(indice['IGPM'], label = 'IGPM Acumulado')
 img.plot(indice['IPCA'], label = 'IPCA Acumulado')
 img.plot(indice['IPPA'], label = 'IPPA Acumulado')
-img.set(xlabel='Ano', ylabel='Inflação (%)',
+img.set(xlabel='Ano', ylabel='Inflação Acumulada (%)',
         title='Inflação Acumulada desde os anos 2000')
 img.grid()
 plt.legend()
 plt.show()
-
