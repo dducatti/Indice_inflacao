bdfe1a4cf7411de6a8fca10762c097bfeb3f724c
Diego Ducatti <118085900+dducatti@users.noreply.github.com>
2023-05-27 12:13:01 -0300

    Update inflação_acumulada.py

--- inflação_acumulada.py
+++ inflação_acumulada.py
@@ -32,7 +32,7 @@ def ipca_acum(p=len(ipca), start = '2000-01-01'):
     ipca['IPCAindex'] = (1 + ipca['IPCA'] / 100)
     acum = pd.DataFrame(ipca[-p:], columns=list(['IPCAindex']))
     acum_produto = acum.cumprod()[-1:]
-    f = (acum_produto.iloc[acum_produto.shape[0]-1, 0] - 1) * 100
+    f = ((acum_produto.iloc[acum_produto.shape[0]-1, 0] - 1) * 100).round(2)
     return f
 
     
@@ -55,7 +55,7 @@ def igpm_acum(p=len(igpm), start = '2000-01-01'):
     igpm['IGPMindex'] = (1 + igpm['IGPM'] / 100)
     acum = pd.DataFrame(igpm[-p:], columns=list(['IGPMindex']))
     acum_produto = acum.cumprod()[-1:]
-    f = (acum_produto.iloc[acum_produto.shape[0]-1, 0] - 1) * 100
+    f = ((acum_produto.iloc[acum_produto.shape[0]-1, 0] - 1) * 100).round(2)
     return f
 
 
@@ -78,7 +78,7 @@ def ippa_acum(p=len(ippa), start = '2000-01-01'):
     ippa['IPPAindex'] = (1 + ippa['IPPA'] / 100)
     acum = pd.DataFrame(ippa[-p:], columns=list(['IPPAindex']))
     acum_produto = acum.cumprod()[-1:]
-    f = (acum_produto.iloc[acum_produto.shape[0]-1, 0] - 1) * 100
+    f = ((acum_produto.iloc[acum_produto.shape[0]-1, 0] - 1) * 100).round(2)
     return f
 
 
@@ -101,7 +101,7 @@ def ipa_acum(p=len(igpm), start = '2000-01-01'):
     ipa['IPAindex'] = (1 + ipa['IPA'] / 100)
     acum = pd.DataFrame(ipa[-p:], columns=list(['IPAindex']))
     acum_produto = acum.cumprod()[-1:]
-    f = (acum_produto.iloc[acum_produto.shape[0]-1, 0] - 1) * 100
+    f = ((acum_produto.iloc[acum_produto.shape[0]-1, 0] - 1) * 100).round(2)
     return f
 
 
@@ -109,67 +109,44 @@ def ipa_acum(p=len(igpm), start = '2000-01-01'):
 p1 = 12
 p2 = 24
 p3 = 120
+p4 = '01/01/2000...'
 
-### IPCA
-print('-'*40)
-a = ipca_acum(p1)
-b = ipca_acum(p2)
-c = ipca_acum(p3)
-d = ipca_acum()
-print(f'IPCA Acumulado em {p1}m:\t\t{a:.2f}%')
-print(f'IPCA Acumulado em {p2}m:\t\t{b:.2f}%')
-print(f'IPCA Acumulado em {p3}m:\t\t{c:.2f}%')
-print(f'IPCA Acumulado 01-01-2000:\t{d:.2f}%')
-print('-'*40)
-### IGPM
-e = igpm_acum(12)
-f = igpm_acum(24)
-g = igpm_acum(120)
-h = igpm_acum()
-print(f'IGPM Acumulado em {p1}m:\t\t{e:.2f}%')
-print(f'IGPM Acumulado em {p2}m:\t\t{f:.2f}%')
-print(f'IGPM Acumulado em {p3}m:\t\t{g:.2f}%')
-print(f'IGPM Acumulado 01-01-2000:\t{h:.2f}%')
-print('-'*40)
+## IPCA :433
+IPCA = [ipca_acum(p1),
+        ipca_acum(p2),
+        ipca_acum(p3),
+        ipca_acum()]
+
+## IGPM :189
+IGPM = [igpm_acum(p1),
+        igpm_acum(p2),
+        igpm_acum(p3),
+        igpm_acum()]
+
 ## IPP-m :225
-i = ippa_acum(12)
-j = ippa_acum(24)
-k = ippa_acum(120)
-l = ippa_acum()
-print(f'IPPA Acumulado em {p1}m:\t\t{i:.2f}%')
-print(f'IPPA Acumulado em {p2}m:\t\t{j:.2f}%')
-print(f'IPPA Acumulado em {p3}m:\t\t{k:.2f}%')
-print(f'IPPA Acumulado 01-01-2000:\t{l:.2f}%')
-print('-'*40)
-#IPA-m :7450
-m = ipa_acum(p1)
-n = ipa_acum(p2)
-o = ipa_acum(p3)
-p = ipa_acum()
-print(f'IPA Acumulado em {p1}m:\t\t{m:.2f}%')
-print(f'IPA Acumulado em {p2}m:\t\t{n:.2f}%')
-print(f'IPA Acumulado em {p3}m:\t\t{o:.2f}%')
-print(f'IPA Acumulado 01-01-2000:\t{p:.2f}%')
+IPP = [ippa_acum(p1),
+       ippa_acum(p2),
+       ippa_acum(p3),
+       ippa_acum()]
+
+## IPA-m :7450
+IPA = [ipa_acum(p1),
+       ipa_acum(p2),
+       ipa_acum(p3),
+       ipa_acum()]
 
 
+nomes = ('IPCA %',
+         'IGPM %',
+         'IPP %',
+         'IPA %')
 
+indice = ('Acum.  ' + str(p1) + ' meses',
+          'Acum.  ' + str(p2) + ' meses',
+          'Acum. ' + str(p3) + ' meses',
+          p4)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+result = pd.DataFrame(data=(IPCA, IGPM, IPP, IPA),
+                      columns=indice,
+                      index=nomes).T
+print(result)
